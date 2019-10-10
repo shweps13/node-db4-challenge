758c0a92fdfc54d326890dda45bc138c047ccc34
Heorhii Siburov <g.siburov@gmail.com>
2019-10-10 15:37:12 -0700

    knex inited and parameters was added

---
+++ recipes/recipes-router.js
@@ -0,0 +1,19 @@
+const express = require('express');
+
+// const Schemes = require('./scheme-model.js');
+
+const router = express.Router();
+
+// router.get('/', (req, res) => {
+//   Schemes.find()
+//   .then(schemes => {
+//     res.json(schemes);
+//   })
+//   .catch(err => {
+//     res.status(500).json({ message: 'Failed to get schemes' });
+//   });
+// });
+
+
+
+module.exports = router;
--- server.js
+++ server.js
@@ -2,15 +2,17 @@ const express = require('express');
 const helmet = require('helmet');
 
 const server = express();
+const recipesRouter = require('./recipes/recipes-router.js');
 
 server.use(helmet());
 server.use(express.json());
+server.use('/api/recipes', recipesRouter);
 
 server.get('/', (req, res) => {
     const nameInsert = req.teamName ? ` ${req.teamName}` : '';
     const yourData = process.env
     res.send(`
-    <h2>Heorhii Hubs API</h2>
+    <h2>Heorhii Recipe Book API</h2>
     <p>Hello ${yourData.USER}</p>
     <p>Welcome ${nameInsert} to the Heorhii Hubs API on port ${yourData.PORT}</p>
     `);
